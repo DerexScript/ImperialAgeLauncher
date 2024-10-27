85a9458507c57754108699aa40567c83019d3985
DerexScript <derex@outlook.com.br>
2024-10-27 17:13:09 -0300

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace ImperialAgeLauncher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(160, 149);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 300);
+            this.panel1.TabIndex = 9;
+            this.panel1.Visible = false;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox9.Image = global::ImperialAgeLauncher.Properties.Resources.gear;
+            this.pictureBox9.Location = new System.Drawing.Point(431, 309);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox9.TabIndex = 8;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Click += new System.EventHandler(this.pictureBox9_Click);
+            this.pictureBox9.MouseEnter += new System.EventHandler(this.PictureBox9_MouseEnter);
+            this.pictureBox9.MouseLeave += new System.EventHandler(this.PictureBox9_MouseLeave);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox5.Image = global::ImperialAgeLauncher.Properties.Resources.exit1;
+            this.pictureBox5.Location = new System.Drawing.Point(667, 12);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            this.pictureBox5.MouseEnter += new System.EventHandler(this.pictureBox5_Enter);
+            this.pictureBox5.MouseLeave += new System.EventHandler(this.pictureBox5_Leave);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox4.Image = global::ImperialAgeLauncher.Properties.Resources.jogar1;
+            this.pictureBox4.Location = new System.Drawing.Point(268, 296);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(157, 57);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            this.pictureBox4.MouseEnter += new System.EventHandler(this.pictureBox4_Enter);
+            this.pictureBox4.MouseLeave += new System.EventHandler(this.pictureBox4_Leave);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox3.Image = global::ImperialAgeLauncher.Properties.Resources.discord;
+            this.pictureBox3.Location = new System.Drawing.Point(362, 378);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(142, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            this.pictureBox3.MouseEnter += new System.EventHandler(this.pictureBox3_Enter);
+            this.pictureBox3.MouseLeave += new System.EventHandler(this.pictureBox3_Leave);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Image = global::ImperialAgeLauncher.Properties.Resources.website;
+            this.pictureBox2.Location = new System.Drawing.Point(186, 378);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(142, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.pictureBox2.MouseEnter += new System.EventHandler(this.pictureBox2_Enter);
+            this.pictureBox2.MouseLeave += new System.EventHandler(this.pictureBox2_Leave);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::ImperialAgeLauncher.Properties.Resources.Design_sem_nome__24_;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(707, 486);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 481);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImperialAgeLauncher
+{
+    public partial class Form1 : Form
+    {
+        private bool dragging = false;
+        private Point dragCursorPoint;
+        private Point dragFormPoint;
+        private int rotationAngle = 0;
+        private Timer rotationTimer;
+        
+
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(
+            int nLeftRect,
+            int nTopRect,
+            int nRightRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
+        );
+        
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, this.Width, this.Height, 35, 35)); // Altere 30 para ajustar o grau de arredondamento
+
+            // Inicializar o Timer
+            rotationTimer = new Timer();
+            rotationTimer.Interval = 10; // Intervalo para um efeito de rotação suave
+            rotationTimer.Tick += RotationTimer_Tick;
+
+            // Conecte os eventos do PictureBox
+            pictureBox1.MouseDown += pictureBox1_MouseDown;
+            pictureBox1.MouseMove += pictureBox1_MouseMove;
+            pictureBox1.MouseUp += pictureBox1_MouseUp;
+
+            // Conectar os eventos de rotação ao pictureBox9
+            pictureBox9.MouseEnter += PictureBox9_MouseEnter;
+            pictureBox9.MouseLeave += PictureBox9_MouseLeave;
+            pictureBox9.Paint += PictureBox9_Paint;
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox2.Parent = pictureBox1;
+            pictureBox3.Parent = pictureBox1;
+            pictureBox4.Parent = pictureBox1;
+            pictureBox5.Parent = pictureBox1;
+            pictureBox9.Parent = pictureBox1;
+
+        }
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Quando o mouse é pressionado, iniciamos o arrasto
+            dragging = true;
+            // Pega a posição atual do cursor e do formulário
+            dragCursorPoint = Cursor.Position;
+            dragFormPoint = this.Location;
+        }
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Se estiver arrastando, mova o formulário
+            if (dragging)
+            {
+                Point diff = Point.Subtract(Cursor.Position, new Size(dragCursorPoint));
+                this.Location = Point.Add(dragFormPoint, new Size(diff));
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            // Finaliza o arrasto quando o botão do mouse é solto
+            dragging = false;
+        }
+        //botao website
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("https://imperialage.com.br/") { UseShellExecute = true });
+        }
+        private void pictureBox2_Enter(object sender, EventArgs e)
+        {
+            
+            pictureBox2.Image = Properties.Resources.website1;
+
+        }
+        private void pictureBox2_Leave(object sender, EventArgs e)
+        {
+            
+            pictureBox2.Image = Properties.Resources.website;
+        }
+
+        //botao discord
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("https://discord.gg/AsY7kv8H") { UseShellExecute = true });
+        }
+        private void pictureBox3_Enter(object sender, EventArgs e)
+        {
+            //pictureBox3.Image = (Bitmap)new ImageConverter().ConvertFrom(Properties.Resources.avancar2);
+            pictureBox3.Image = Properties.Resources.discord1;
+
+        }
+        private void pictureBox3_Leave(object sender, EventArgs e)
+        {
+            //pictureBox3.Image = (Bitmap)new ImageConverter().ConvertFrom(Properties.Resources.avancar1);
+            pictureBox3.Image = Properties.Resources.discord;
+        }
+        
+        //botao exit
+        private void pictureBox5_Enter(object sender, EventArgs e)
+        {
+            pictureBox5.Image = Properties.Resources.exit2;
+        }
+        private void pictureBox5_Leave(object sender, EventArgs e)
+        {
+            pictureBox5.Image = Properties.Resources.exit1;
+        }
+
+        //botao Jogar
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox4_Enter(object sender, EventArgs e)
+        {
+            pictureBox4.Image = Properties.Resources.jogar2;
+        }
+        private void pictureBox4_Leave(object sender, EventArgs e)
+        {
+            pictureBox4.Image = Properties.Resources.jogar1;
+        }
+
+        //botao exit
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show(
+                "Você tem certeza que deseja fechar?",
+                "Confirmar saída",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+            );
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+        //botao gear
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void PictureBox9_MouseEnter(object sender, EventArgs e)
+        {
+            rotationAngle = 0; // Resetar o ângulo
+            rotationTimer.Start(); // Iniciar o Timer para rotacionar
+        }
+
+        private void PictureBox9_MouseLeave(object sender, EventArgs e)
+        {
+            rotationTimer.Stop(); // Parar o Timer ao sair do mouse
+            pictureBox9.Invalidate(); // Atualizar o PictureBox para redefinir o ângulo
+        }
+
+        private void RotationTimer_Tick(object sender, EventArgs e)
+        {
+            rotationAngle += 5; // Incremento do ângulo de rotação
+            if (rotationAngle >= 360)
+            {
+                rotationAngle = 0; // Resetar após completar uma volta
+            }
+            pictureBox9.Invalidate(); // Redesenhar o PictureBox com o novo ângulo
+        }
+
+        private void PictureBox9_Paint(object sender, PaintEventArgs e)
+        {
+            // Rotacionar a imagem do PictureBox
+            if (pictureBox9.Image != null)
+            {
+                e.Graphics.TranslateTransform(pictureBox9.Width / 2, pictureBox9.Height / 2);
+                e.Graphics.RotateTransform(rotationAngle);
+                e.Graphics.TranslateTransform(-pictureBox9.Width / 2, -pictureBox9.Height / 2);
+                e.Graphics.DrawImage(pictureBox9.Image, new Rectangle(0, 0, pictureBox9.Width, pictureBox9.Height));
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
